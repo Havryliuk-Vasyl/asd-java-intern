6ec1c6d26aca439b734ded6793480bb1ade21284
Havryliuk-Vasyl <vasiagavryluk20@gmail.com>
2024-11-14 16:51:08 +0200

    Added Optional to better manage the absence of values
    Added Optional to better manage the absence of values, moved DateTimeFormatter and improved throwing exceptions.

--- src/main/java/team/asd/service/ConverterService.java
+++ src/main/java/team/asd/service/ConverterService.java
@@ -3,8 +3,10 @@ package team.asd.service;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.Optional;
 
 public class ConverterService implements IsConverterService {
+	private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
 	@Override
 	public String convertIntegerIntoString(Integer value) {
@@ -13,47 +15,45 @@ public class ConverterService implements IsConverterService {
 
 	@Override
 	public Integer convertStringIntoInteger(String value) {
-		if (value == null)
-			return null;
-
-		int number;
-		try {
-			number = Integer.parseInt(value);
-		}
-		catch (NumberFormatException e){
-			throw new NumberFormatException("String does not contain numbers");
-		}
-		return number;
+		return Optional.ofNullable(value)
+				.map(val -> {
+					int number;
+					try {
+						return number = Integer.parseInt(val);
+					} catch (NumberFormatException e) {
+						throw new NumberFormatException(e.getMessage());
+					}
+				})
+				.orElse(null);
 	}
 
 	@Override
 	public Double convertStringIntoDouble(String value) {
-		if (value == null)
-			return null;
-
-		double number;
-		try {
-			number = Double.parseDouble(value);
-		}
-		catch (NumberFormatException e){
-			throw new NumberFormatException("String does not contain numbers");
-		}
-		return number;
+		return Optional.ofNullable(value)
+				.map(val -> {
+					double number;
+					try {
+						number = Double.parseDouble(val);
+					} catch (NumberFormatException e) {
+						throw new NumberFormatException(e.getMessage());
+					}
+					return number;
+				})
+				.orElse(null);
 	}
 
 	@Override
 	public LocalDate convertStringIntoLocalDate(String dateString) throws DateTimeParseException {
-		if (dateString == null)
-			return null;
-
-		LocalDate date;
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		try {
-			date = LocalDate.parse(dateString, formatter);
-		}
-		catch (DateTimeParseException e){
-			throw new DateTimeParseException("Cannot convert String to LocalDate", e.getParsedString(), e.getErrorIndex());
-		}
-		return date;
+		return Optional.ofNullable(dateString)
+				.map(value -> {
+					LocalDate date;
+					try {
+						date = LocalDate.parse(value, formatter);
+					} catch (DateTimeParseException e) {
+						throw new DateTimeParseException(e.getMessage(), e.getParsedString(), e.getErrorIndex());
+					}
+					return date;
+				})
+				.orElse(null);
 	}
 }
